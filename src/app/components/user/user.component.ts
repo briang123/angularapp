04b812cb46570d730c7ecd1cba6f5dd1ffa8a958
briang123 <briang.sandiego@gmail.com>
2019-02-08 12:59:27 -0800

    messing with typescript

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,14 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-  firstName = 'John';
-  lastName = 'Doe';
-  age = 30;
-  address = {
-    city: 'San Diego',
-    state: 'California',
-    street: '50 main street'
-  };
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
 
-  constructor() {}
+  foo: any;
+  hasKids: boolean;
+  numberArray: number[];
+  stringArray: string[];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unusable: void;
+  u: undefined;
+  n: null;
+
+  constructor() {
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.age = 30;
+
+    this.address = {
+      city: 'San Diego',
+      state: 'California',
+      street: '50 main street'
+    };
+
+    this.foo = true;
+    this.hasKids = true;
+    this.numberArray = [1, 2, 3];
+    this.stringArray = ['Hello', 'World'];
+    this.mixedArray = [1, 'Hello', undefined];
+    this.myTuple = ['Hello', 1, true];
+    this.unusable = undefined;
+    this.u = undefined;
+    this.n = null;
+
+    console.log('this.addNumbers(1,2): ', this.addNumbers(1, 2));
+  }
+
+  addNumbers(n1: number, n2: number): number {
+    return n1 + n2;
+  }
 }
