a7643aa2718fcafd02fe0d89a7cc6a866143e3a8
briang123 <briang.sandiego@gmail.com>
2019-02-08 12:06:57 -0800

    created basic user component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  //template: '<h2>John Doe</h2>'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+  // styles: [
+  //   `
+  //     h2 {
+  //       color: blue;
+  //     }
+  //   `
+  // ]
+})
+export class UserComponent {}
